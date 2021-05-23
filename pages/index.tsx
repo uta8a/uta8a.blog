accd06ab7dda05c177f92f666d8b194475669980
uta8a <noumodulity@gmail.com>
2021-05-23 11:24:19 +0900

    change toppage

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,29 +8,38 @@ type Props = {
 };
 export default function Home(props: Props): JSX.Element {
   return (
-    <div>
-      {props.path.map((e) => {
-        return (
-          <h1
-            key={e}
-            style={{
-              fontSize: '2.25rem',
-              lineHeight: '2.5rem',
-              textAlign: 'center',
-            }}
-          >
-            <Link href={`example/${e}`} passHref>
-              <a>{e}</a>
-            </Link>
-          </h1>
-        );
-      })}
+    <div
+      style={{
+        paddingTop: '4rem',
+        paddingLeft: '4rem',
+      }}
+    >
+      <h1
+        style={{
+          paddingBottom: '2rem',
+          fontSize: '3rem',
+        }}
+      >
+        diaryです
+      </h1>
+      <Link href={'post/'} passHref>
+        <h1
+          style={{
+            paddingBottom: '2rem',
+          }}
+        >
+          /post - 技術的な内容
+        </h1>
+      </Link>
+      <Link href={'diary/'} passHref>
+        <h1
+          style={{
+            paddingBottom: '2rem',
+          }}
+        >
+          /diary - 個人的な日記
+        </h1>
+      </Link>
     </div>
   );
 }
-export const getStaticProps: GetStaticProps<Props> = async () => {
-  const type = 'example';
-  const targetDir = path.join(process.cwd(), 'content', type);
-  const dirs = await fs.readdir(targetDir);
-  return { props: { path: dirs } };
-};
